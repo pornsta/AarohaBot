3e271f1a2f30381ed33da47a29db9fe854c223e7
Botyyyy <107973574+pornsta@users.noreply.github.com>
2023-02-01 23:25:48 +0545

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ async def message_handler(event):
     '''
 
             newbutton = [Button.url('Help🙋',
-                                    f'https://t.me/Mdisk_MovieLink')]
+                                    f'https://t.me/teraboxmovielink')]
 
             await txt.delete()
             result = await event.reply(answer, buttons=newbutton, link_preview=False)
@@ -132,7 +132,7 @@ async def message_handler(event):
         message = f'**Click Here 👇 For "{event.text}"**\n\n[🍿🎬 {str(event.text).upper()}\n🍿🎬 {str("Click me for results").upper()}]({tgraph_result})'
 
         newbutton = [Button.url('How To Watch ❓',
-                                    f'https://t.me/Mdisk_MovieLink')]
+                                    f'https://t.me/teraboxmovielink')]
 
         await txt.delete()
         await asyncio.sleep(0.5)
@@ -144,7 +144,7 @@ async def message_handler(event):
     except Exception as e:
         print(e)
         await txt.delete()
-        result = await event.reply("I am Unable Search,Please Search In @Mdisk_MovieLink 🙏")
+        result = await event.reply("I am Unable Search,Please Search In @teraboxmovielink 🙏")
         await asyncio.sleep(Config.AUTO_DELETE_TIME)
         await event.delete() 
         return await result.delete()
